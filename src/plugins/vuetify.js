d7acd0a919824991aacdc91934243be513d91eb6
Huckleberry K <hawk.kim1974@gmail.com>
2022-09-26 17:25:38 +0900

    changed to use MDI font

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,4 +1,4 @@
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
@@ -16,7 +16,7 @@ const theme = {
 
 export default new Vuetify({
   icons: {
-    iconfont: 'md',
+    iconfont: 'mdi',
   },
   theme: {
     themes: {
